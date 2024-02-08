f6f6a7d2545fb53a3ec00c42285b89e517903938
Ishana0 <ishanakalikiri01@gmailcom>
2024-02-08 11:41:48 +0530

    hoc-render-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css';
 import LikeImage from './components/LikeImage';
 import LikePost from './components/LikePost';
+import LikeImage2 from './components/project-hoc/LikeImage2';
+import LikePost2 from './components/project-hoc/LikePost2';
+import LikeImage3 from './components/project-render/LikeImage3';
+import LikePost3 from './components/project-render/LikePost3';
+import RenderProps from './components/project-render/RenderProps';
 
 function App() {
   return (
     <div>
+      {/* Project HOC */}
       <h3>Some Blog</h3>
       <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+        <LikePost2/>
+        <LikeImage2/>
+      </div>
+
+      <div>
+        {/* Project RenderProps */}
+        <h3>Some more Blogs</h3>
+        <div className='buttons'>
+          <RenderProps render={(count, handleCount) => (<LikePost3 count={count}handleCount={handleCount}/>)}/>
+          <RenderProps render={(count, handleCount) => (<LikeImage3 count={count} handleCount={handleCount}/>)}/>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/project-render/RenderProps.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const RenderProps = (props) => {
+    const { render } = props;
+
+    const [count, setCount] = React.useState(0);
+
+    const handleCount = () => {
+        setCount((prev) => prev + 1);
+    };
+    return<div>
+        {render(count, handleCount)}
+    </div>;
+};
+export default RenderProps;
